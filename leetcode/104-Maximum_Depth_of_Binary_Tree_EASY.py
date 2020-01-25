46ceb429b839ea2c7ed49f3fb5bbf96f8c067b57
Anthony Burdi <anthonyburdi@gmail.com>
2020-01-25 13:21:10 -0500

    added top down solution

--- leetcode/104-Maximum_Depth_of_Binary_Tree_EASY.py
+++ leetcode/104-Maximum_Depth_of_Binary_Tree_EASY.py
@@ -25,29 +25,56 @@ class TreeNode:
         self.right = None
 
 class Solution:
-    def maxDepth(self, root: TreeNode) -> int:
 
+    def __init__(self):
+        self.answer = 0
+
+    def maxDepthBottomUp(self, root: TreeNode) -> int:
+
+        # Bottom up
         if not root:
             return 0
 
-        left_ans = self.maxDepth(root.left)
-        right_ans = self.maxDepth(root.right)
+        left_ans = self.maxDepthBottomUp(root.left)
+        right_ans = self.maxDepthBottomUp(root.right)
 
         return max(left_ans, right_ans) + 1
 
+    def maxDepthTopDown(self, root: TreeNode, depth: int) -> int:
+        # Top down
+        if root == None:
+            return 0
+
+        left_ans = self.maxDepthTopDown(root.left, depth + 1)
+        right_ans = self.maxDepthTopDown(root.right, depth + 1)
+        return max(left_ans, right_ans) + 1
+
+
+import unittest
+
+class TestMaxDepth(unittest.TestCase):
+
+    def setUp(self):
+        self.solution = Solution()
+        self.n3 = TreeNode(3)
+        self.n9 = TreeNode(9)
+        self.n20 = TreeNode(20)
+        self.n15 = TreeNode(15)
+        self.n7 = TreeNode(7)
+
+        self.n3.left = self.n9
+        self.n3.right = self.n20
+        self.n20.left = self.n15
+        self.n20.right = self.n7
+
+    def test_example_1(self):
+        answer = 3
+        self.assertEqual(self.solution.maxDepthBottomUp(self.n3), answer)
+        self.assertEqual(
+            self.solution.maxDepthTopDown(self.n3, self.solution.answer),
+            answer
+        )
+
 
 if __name__ == '__main__':
-    s = Solution()
-
-    n3 = TreeNode(3)
-    n9 = TreeNode(9)
-    n20 = TreeNode(20)
-    n15 = TreeNode(15)
-    n7 = TreeNode(7)
-
-    n3.left = n9
-    n3.right = n20
-    n20.left = n15
-    n20.right = n7
-
-    print("Example 1: should be 3", s.maxDepth(n3))
+    unittest.main()
