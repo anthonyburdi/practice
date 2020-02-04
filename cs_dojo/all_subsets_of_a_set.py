7caefffbffc93db5ad42382c86acefd26b8de196
Anthony Burdi <anthonyburdi@gmail.com>
2020-02-04 18:43:39 -0500

    added solution

---
+++ cs_dojo/all_subsets_of_a_set.py
@@ -0,0 +1,52 @@
+# all_subsets_of_a_set.py
+
+# Facebook Coding Interview Question and Answer #1: All Subsets of a Set
+
+# https://www.youtube.com/watch?v=bGC2fNALbNU
+
+from typing import List
+
+subsets = []
+
+def all_subsets(given_array: List, subsets: List) -> None:
+
+    subset = [None] * len(given_array)
+
+    helper(given_array, subset, 0, subsets)
+
+    return subsets
+
+
+def helper(given_array: List, subset: List, i: int, subsets: List) -> None:
+
+    if i == len(given_array):
+        # print(subset)
+        # remove None
+        temp_subset = [n for n in subset if n is not None]
+        subsets.append(temp_subset)
+    else:
+        # don't include item
+        subset[i] = None
+        helper(given_array, subset, i + 1, subsets)
+        # include item
+        subset[i] = given_array[i]
+        helper(given_array, subset, i + 1, subsets)
+
+
+import unittest
+
+class TestAllSubsets(unittest.TestCase):
+
+    def test_given_example(self):
+
+        given_array = [1, 2]
+
+        self.assertEqual(
+            all_subsets(given_array, subsets).sort(),
+            # [[None, None],[1, None],[None, 2],[1, 2]].sort()
+            [[], [1], [2], [1, 2]].sort()
+        )
+
+
+if __name__ == '__main__':
+    unittest.main()
